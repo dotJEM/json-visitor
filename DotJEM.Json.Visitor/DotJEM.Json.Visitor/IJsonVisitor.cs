38fa8cf2554de49a6b619c399eb2fee01a3f713b
Jens Melgaard <admin@it-links.dk>
2016-10-25 14:10:55 +0200

    Started adding some documentation comments

--- DotJEM.Json.Visitor/DotJEM.Json.Visitor/IJsonVisitor.cs
+++ DotJEM.Json.Visitor/DotJEM.Json.Visitor/IJsonVisitor.cs
@@ -2,6 +2,11 @@ using Newtonsoft.Json.Linq;
 
 namespace DotJEM.Json.Visitor
 {
+    /// <summary>
+    /// Basic interface for implementing a JToken visitor, the visitor specifies a context implementation.
+    /// One implementation of the context could be used to track the location in the current full Json structure.
+    /// </summary>
+    /// <typeparam name="TContext"></typeparam>
     public interface IJsonVisitor<in TContext> where TContext: IJsonVisitorContext<TContext>
     {
         void DoAccept(JToken json, TContext context);
